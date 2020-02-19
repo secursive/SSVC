058472e6f9601ce416d94296d96baa5614d35324
Secursive | Muhammad Akbar <akbar@secursive.com>
2020-02-18 23:48:42 -0800

    Update ssvc_tests.py

--- src/ssvc_tests.py
+++ src/ssvc_tests.py
@@ -2,7 +2,7 @@
 '''
 file: ssvc_tests
 author: secursive
-reference: https://blog.secursive.com/posts/1-Critical-Look-Stakeholder-Specific-Vulnerability-Categorization-SSVC.html
+reference: https://blog.secursive.com/posts/critical-first-look-stakeholder-specific-vulnerability-categorization-ssvc/
 '''
 
 import unittest
@@ -132,7 +132,7 @@ class SSVC_Tests(unittest.TestCase):
 			pprint.pprint(div_max_examples, stream=f_div)
 
 def main():
-	print('\nTest Details: https://blog.secursive.com/posts/1-Critical-Look-Stakeholder-Specific-Vulnerability-Categorization-SSVC.html')
+	print('\nTest Details: https://blog.secursive.com/posts/critical-first-look-stakeholder-specific-vulnerability-categorization-ssvc/')
 	unittest.main()
 
 if __name__ == '__main__':
